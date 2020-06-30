0b5b6383d41ece083c75b4d4f69738df07fd9fa1
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-29 20:32:22 -0400

    initial scrapping setup

---
+++ README.md
@@ -0,0 +1,55 @@
+# web_scraper <!-- omit in toc -->
+
+- [Overview](#overview)
+- [Goals](#goals)
+- [Libraries](#libraries)
+- [Code Showcase](#code-showcase)
+
+<br>
+
+## Overview
+
+_**Web Scrapper** create a web scrapper with Python._
+
+<br>
+
+### Steps
+
+- _First step_
+
+<br>
+
+### Libraries
+
+> Supporting libraries and dependencies, and their role in the project.
+
+|    Library    | Description      |
+| :-----------: | :--------------- |
+| BeautifulSoup | _Web Scrapping._ |
+
+<br>
+
+### Title
+
+#### Tittle
+
+> Details here.
+
+- section
+
+- section
+
+#### Title
+
+> Details here.
+
+```structure
+
+Main
+|__ assets/
+      |__ fonts
+      |__ graphics
+      |__ images
+      |__ mockups
+
+```
--- scraper.py
+++ scraper.py
@@ -1,2 +1,30 @@
 import urllib.request
 from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        # urlopen() makes a request to the site and
+        # returns a responce object
+        # with html from the site we passed in.
+        res = urllib.request\
+            .urlopen(self.site)
+        html = res.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        # finding the target
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            # print("\n" + url)
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
+
+
+url_to_scrape = 'http://econpy.pythonanywhere.com/ex/001.html'
+scraper_results = Scraper(url_to_scrape)
+scraper_results.scrape()
